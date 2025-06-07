75f1953359f294cefa568fe7f1ab6656994ed584
Thebolachas <gustavocostaduarte@live.com>
2025-06-07 13:14:29 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
-// Vite config file
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: 'dist' }
+});
